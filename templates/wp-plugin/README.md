4f86d5ed4fe8921214e3408fc9001563b932fa97
Candace Johnson <candace@candacejohnsondesigns.com>
2025-04-08 08:40:31 -0700

    Added basic files for creating and initializing a Wordpress plugin

---
+++ templates/wp-plugin/README.md
@@ -0,0 +1,20 @@
+# WordPress Plugin Starter
+
+This plugin was scaffolded with the `cj-create` CLI tool.
+
+## Features
+
+- Basic plugin header and safety checks
+- Easy to extend into a full-featured plugin
+- Includes GitHub Actions for PHP linting
+- Includes GitHub Issue Templates
+
+## Setup
+
+1. Rename `plugin-name.php` to match your plugin slug.
+2. Update headers and logic.
+3. Build your plugin features.
+
+## License
+
+GPL-2.0+
---
+++ templates/wp-plugin/plugin-name.php
@@ -0,0 +1,25 @@
+/**
+ * Plugin Name
+ *
+ * @package           PluginPackage
+ * @author            [[AUTHOR_NAME]]
+ * @copyright         2019 Candace Johnson Designs
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       [[PLUGIN_NAME]]
+ * Plugin URI:        [[PLUGIN_URI]]
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            [[AUTHOR_NAME]]
+ * Author URI:        [[AUTHOR_URI]]
+ * Text Domain:       [[PLUGIN_SLUG]]
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
